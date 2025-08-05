3563a1d3d6065c52a1ca25b96d9c660ef62654da
Kaydeth <Kaydeth@gmail.com>
2025-08-04 21:07:17 -0400

    Updated for 42.11.0 to support a recipe filter when you open the Build and Craft menu. Also includes support for the building menu hot key.

--- Contents/mods/SecondPlayerBuildMenuFix/42/media/lua/client/Entity/ISEntityUIKayPatch.lua
+++ Contents/mods/SecondPlayerBuildMenuFix/42/media/lua/client/Entity/ISEntityUIKayPatch.lua
@@ -3,19 +3,19 @@ require "Entity/ISEntityUI"
 ISEntityUIKayPatch = {}
 
 ISEntityUIKayPatch.OpenBuildWindowOrig = ISEntityUI.OpenBuildWindow
-function ISEntityUI.OpenBuildWindow(_player, _isoObject, _queryOverride, _ignoreFindSurface)
+function ISEntityUI.OpenBuildWindow(_player, _isoObject, _queryOverride, _ignoreFindSurface, recipe)
     --Skip the close all windows call when opening the building menu so that each player can open
     --the menu without closing everybody else's windows
     ISEntityUIKayPatch.ignoreClose = true;
-    ISEntityUIKayPatch.OpenBuildWindowOrig(_player, _isoObject, _queryOverride, _ignoreFindSurface)
+    ISEntityUIKayPatch.OpenBuildWindowOrig(_player, _isoObject, _queryOverride, _ignoreFindSurface, recipe)
 end
 
 ISEntityUIKayPatch.OpenHandcraftWindowOrig = ISEntityUI.OpenHandcraftWindow
-function ISEntityUI.OpenHandcraftWindow(_player, _isoObject, _queryOverride, _ignoreFindSurface)
+function ISEntityUI.OpenHandcraftWindow(_player, _isoObject, _queryOverride, _ignoreFindSurface, recipe, itemString)
     --Skip the close all windows call when opening the crafting menu so that each player can open
     --the menu without closing everybody else's windows
     ISEntityUIKayPatch.ignoreClose = true;
-    ISEntityUIKayPatch.OpenHandcraftWindowOrig(_player, _isoObject, _queryOverride, _ignoreFindSurface)
+    ISEntityUIKayPatch.OpenHandcraftWindowOrig(_player, _isoObject, _queryOverride, _ignoreFindSurface, recipe, itemString)
 end
 
 ISEntityUIKayPatch.ignoreClose = false;
--- Contents/mods/SecondPlayerBuildMenuFix/42/media/lua/client/ISUI/Building/ISBuildWindowKayPatch.lua
+++ Contents/mods/SecondPlayerBuildMenuFix/42/media/lua/client/ISUI/Building/ISBuildWindowKayPatch.lua
@@ -4,12 +4,12 @@ ISBuildWindow.instance = {};
 
 function ISBuildWindow:isKeyConsumed(key)
     --The keyboard should only effect the open menus of the first player
-    return self.playerNum == 0 and key == Keyboard.KEY_ESCAPE
+    return self.playerNum == 0 and (key == Keyboard.KEY_ESCAPE or getCore():isKey("Building UI", key))
 end
 
 function ISBuildWindow:onKeyRelease(key)
     --The keyboard should only effect the open menus of the first player
-    if self:isVisible() and self.playerNum == 0 and key == Keyboard.KEY_ESCAPE then
+    if self:isVisible() and self.playerNum == 0 and (key == Keyboard.KEY_ESCAPE or getCore():isKey("Building UI", key)) then
         self:close()
         self:removeFromUIManager();
         return
